7c21e23e4468b25c7db43ae0cd4a01e327b9e25f
Diogo Fontes <fontesdgfranca@gmail.com>
2024-04-27 16:01:02 -0300

    feat: Configurando prisma

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return ' World!';
+  constructor(private prisma: PrismaService) {} // Injete o serviço Prisma
+
+  async getUsers() {
+    return this.prisma.user.findMany(); // Use o Prisma para acessar os usuários no banco de dados
+  }
+
+  async createUser(name: string) {
+    return this.prisma.user.create({
+      data: {
+        email: name,
+      },
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appService = app.get(AppService); // Injeta o AppService
+  await appService.createUser('Diogo');
   await app.listen(3000);
 }
 bootstrap();
