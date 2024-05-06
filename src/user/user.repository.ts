34445d9d75459fa5a621c1bb60dcfd2a5c06deb4
christiandoramo <christiandoramo@gmail.com>
2024-05-06 19:59:03 -0300

    asdadad

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,6 +2,6 @@ import { User } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 export abstract class UserRepository {
     abstract create(user: CreateUserDto): Promise<User>;
-    abstract findById(id: string): Promise<User>;
+    abstract findById(id: string): Promise<Usefdfd>;
     abstract findByEmail(email: string): Promise<User | null>;
 }
